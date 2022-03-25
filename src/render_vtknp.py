384e6bba75c586b2e2a979ae234a0ce96d87bbce
ChengguiSun <30328670+ChengguiSun@users.noreply.github.com>
2022-03-25 00:28:09 -0600

    update import framework of render_vtknp.py

--- src/render_vtknp.py
+++ src/render_vtknp.py
@@ -2,63 +2,58 @@
 
 import numpy as np
 import scipy.io
-import vtkmodules.vtkInteractionStyle
-from vtkmodules.vtkCommonColor import vtkNamedColors
-from vtkmodules.vtkCommonDataModel import vtkPiecewiseFunction
-from vtkmodules.vtkIOImage import vtkImageImport
-from vtkmodules.vtkRenderingCore import (
-    vtkColorTransferFunction,
-    vtkRenderWindow,
-    vtkRenderWindowInteractor,
-    vtkRenderer,
-    vtkVolume,
-    vtkVolumeProperty
-)
-from vtkmodules.vtkRenderingVolume import vtkFixedPointVolumeRayCastMapper
-from vtkmodules.vtkRenderingVolumeOpenGL2 import vtkOpenGLRayCastImageDisplayHelper
+import vtk
 
 # import data from mat file
 mat = scipy.io.loadmat('./Data/Code_and_Data_for_Students/Set1/Neutral1/T10-NA.mat')
 arr = mat['Vertebra'][0,0][1]
-data_arr = arr.astype(np.uint8)  
+data_arr = arr.astype(np.uint8)  # uint8 max value 255
 
 # imports raw data and stores it.
-dataImporter = vtkImageImport()
+dataImporter = vtk.vtkImageImport()
 data_string = data_arr.tobytes() # array is converted to a string of chars and imported.
-dataImporter.CopyImportVoidPointer(data_string, len(data_string))
+dataImporter.CopyImportVoidPointer(data_string, len(data_string)) # Import data and make an internal copy
 dataImporter.SetDataScalarTypeToUnsignedChar() # data set to unsigned char (uint8)
 dataImporter.SetNumberOfScalarComponents(1) 
 dataImporter.SetDataExtent(0, 479, 0, 639, 0, 1199) # 3d dimension
+# dataImporter.SetDataSpacing(3.2,3.2,1.5)
 dataImporter.SetWholeExtent(0, 479, 0, 639, 0, 1199)
+# dataImporter.update()
 
-alphaChannelFunc = vtkPiecewiseFunction() # store transparency-values
+alphaChannelFunc = vtk.vtkPiecewiseFunction() # store transparency-values
+# alphaChannelFunc.AddPoint(0, 0.0)
+# alphaChannelFunc.AddPoint(50, 0.05)
+# alphaChannelFunc.AddPoint(100, 0.1)
+# alphaChannelFunc.AddPoint(150, 0.2)
+# alphaChannelFunc.AddPoint(255, 0.3)
 alphaChannelFunc.AddPoint(0, 0.0)
-alphaChannelFunc.AddPoint(50, 0.05)
+alphaChannelFunc.AddPoint(80, 0.0)
 alphaChannelFunc.AddPoint(100, 0.1)
-alphaChannelFunc.AddPoint(150, 0.2)
+alphaChannelFunc.AddPoint(250, 0.2)
 
-colors = vtkNamedColors() # stores color data
-colorFunc = vtkColorTransferFunction()
+colors = vtk.vtkNamedColors() # stores color data
+colorFunc = vtk.vtkColorTransferFunction()
 colorFunc.AddRGBPoint(50, 1.0, 0.0, 0.0)
 colorFunc.AddRGBPoint(100, 0.0, 1.0, 0.0)
 colorFunc.AddRGBPoint(150, 0.0, 0.0, 1.0)
 
-volumeProperty = vtkVolumeProperty() # add properties to volume
+volumeProperty = vtk.vtkVolumeProperty() # add properties to volume
 volumeProperty.SetColor(colorFunc)
 volumeProperty.SetScalarOpacity(alphaChannelFunc)
+# volumeProperty.SetInterpolationTypeToLinear()
 
-volumeMapper = vtkFixedPointVolumeRayCastMapper()
+volumeMapper = vtk.vtkFixedPointVolumeRayCastMapper()
 volumeMapper.SetInputConnection(dataImporter.GetOutputPort())
 
-volume = vtkVolume()
+volume = vtk.vtkVolume()
 volume.SetMapper(volumeMapper)
 volume.SetProperty(volumeProperty)
 
 # initialize the renderer and window
-renderer = vtkRenderer()
-renderWin = vtkRenderWindow()
+renderer = vtk.vtkRenderer()
+renderWin = vtk.vtkRenderWindow()
 renderWin.AddRenderer(renderer)
-renderInteractor = vtkRenderWindowInteractor()
+renderInteractor = vtk.vtkRenderWindowInteractor()
 renderInteractor.SetRenderWindow(renderWin)
 
 # add the volume to the renderer ...
